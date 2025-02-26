003c3861a2cf53f7578333cb863fc3f92e8a21d4
Alexander <sasha.izmyloff@mail.ru>
2025-02-26 20:08:56 +0300

    Renaming And Moving + Moon Hide And Seek

---
+++ Assets/_Project/Scripts/MurderMoon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+namespace _Project.Scripts
+{
+    public class MurderMoon : MonoBehaviour
+    {
+        [SerializeField] private Transform moon;
+        [SerializeField] private Transform player;
+        [SerializeField] private LayerMask obstacleLayer;
+        
+        [SerializeField] private UnityEvent<float> onTimerStarted;
+        [SerializeField] private UnityEvent onDetectStarted;
+        [SerializeField] private UnityEvent onPlayerDetected;
+    
+        [SerializeField, Min(0)] private float minTimerDuration = 1;
+        [SerializeField, Min(0)] private float maxTimerDuration = 2;
+
+        private void Start() => RestartTimer();
+
+        private void TryDetect()
+        {
+            onDetectStarted.Invoke();
+            RestartTimer();
+            if (Physics.Linecast(moon.position, player.position, obstacleLayer)) return;
+            onPlayerDetected.Invoke();
+        }
+
+        private void RestartTimer()
+        {
+            var time = Random.Range(minTimerDuration, maxTimerDuration);
+            Invoke(nameof(TryDetect), time);
+            onTimerStarted.Invoke(time);
+        }
+    }
+}
