4f2b2c828496f1ff2fbd5a301c444ad3fb08757f
KGoroshnikov <goroshnikovk1@gmail.com>
2025-03-03 00:08:47 +0300

    Movie

--- Assets/_Project/Scripts/MurderMoon.cs
+++ Assets/_Project/Scripts/MurderMoon.cs
@@ -46,7 +46,7 @@ namespace _Project.Scripts
             Gizmos.DrawLine(moon.position, player.position);
         }
 
-        private void Start() => StartTimer();
+        private void OnEnable() => StartTimer();
 
         private IEnumerator WaitAndExecute(float waitTime, Action action)
         {
